8e2103a4e082f731a5a639467231eb52254da031
pyc-chan <rtrtoioil@gmail.com>
2024-11-15 18:37:32 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 164 ms, Memory: 17284 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+
+        for(int i = 1; i < n; i++){
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+        }
+        
+        System.out.print("비와이");
+        
+    }
+}
