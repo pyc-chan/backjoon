50108385dcae8fb240a35e40a4f9c2a48b7aa9b4
pyc-chan <rtrtoioil@gmail.com>
2024-11-15 18:49:00 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 172 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        
+        switch (n) {
+            case 2018:
+            case 2019:
+            case 2020:
+            case 2021:
+            case 2022:
+                System.out.println("A");
+                break;
+        }
+        
+    }
+}
