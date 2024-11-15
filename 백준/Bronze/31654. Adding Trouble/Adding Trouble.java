f20bdd95c35ff59565074af6c322349d9eb6e726
pyc-chan <rtrtoioil@gmail.com>
2024-11-15 18:53:02 +0900

    [Bronze V] Title: Adding Trouble, Time: 176 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        if(a+b == c){
+            System.out.println("correct!");
+        }else{
+            System.out.println("wrong!");
+        }
+    }
+}
