9da20ee617d508d43ea4552c1abc1fe7a024c9d5
pyc-chan <rtrtoioil@gmail.com>
2024-11-15 18:06:20 +0900

    [Bronze V] Title: A/B, Time: 184 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+        System.out.print(a/b);
+    }
+}
