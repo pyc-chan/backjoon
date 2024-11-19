0a8f70a7b3103c80cca29aa5335a69885f826cdd
pyc-chan <rtrtoioil@gmail.com>
2024-11-19 12:12:36 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Scanner scan = new Scanner(System.in);
+        
+        
+
+
+        System.out.print("|\\_/|\n" +
+                        "|q p|   /}\n" + 
+                        "( 0 )\"\"\"\\\n" + 
+                        "|\"^\"`    |\n" + 
+                        "||_/=\\\\__|");
+        // scan.close();
+    }
+}
