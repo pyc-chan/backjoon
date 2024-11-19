9bf8a3f2a99aa4be64df6d51c0ce47f084caa179
pyc-chan <rtrtoioil@gmail.com>
2024-11-19 12:11:34 +0900

    [Bronze V] Title: 고양이, Time: 92 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Scanner scan = new Scanner(System.in);
+        
+        
+
+
+        System.out.print("\\    /\\\n" +
+                        " )  ( ')\n" + 
+                        "(  /  )\n" +
+                        " \\(__)|");
+        // scan.close();
+    }
+}
