deb58a6622ef0795f3823117a59593d3898d1507
pyc-chan <rtrtoioil@gmail.com>
2024-11-19 12:55:21 +0900

    [Bronze V] Title: A+B - 3, Time: 200 ms, Memory: 17900 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        
+        for(int i = 1; i<= a; i++){
+            int su1 = scan.nextInt();
+            int su2 = scan.nextInt();
+            System.out.println(su1+su2);
+        }
+
+        scan.close();
+    }
+}
