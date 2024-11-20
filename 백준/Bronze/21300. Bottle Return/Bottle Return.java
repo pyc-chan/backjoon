9d98ff253348ede642b9eea44d21586b63dc05ef
pyc-chan <rtrtoioil@gmail.com>
2024-11-20 12:14:07 +0900

    [Bronze V] Title: Bottle Return, Time: 172 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int d = scan.nextInt();
+        int e = scan.nextInt();
+        int f = scan.nextInt();
+        
+        System.out.println((a+b+c+d+e+f)*5);
+        
+
+        scan.close();
+    }
+}
