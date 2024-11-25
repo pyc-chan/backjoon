8c79dabe5562f1b04b7add17ce334ac2ee347d40
pyc-chan <rtrtoioil@gmail.com>
2024-11-25 16:15:57 +0900

    [Bronze V] Title: A+B - 5, Time: 188 ms, Memory: 17888 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int a;
+        int b;
+        while(true){
+            a=scan.nextInt();
+            b=scan.nextInt();
+            if(a==0&&b==0){
+                break;
+            }
+            System.out.println(a+b);
+        }
+        scan.close();
+    }
+}
