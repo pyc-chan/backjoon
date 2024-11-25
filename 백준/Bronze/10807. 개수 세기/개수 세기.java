a64ea0011be8af1a26b0505431d6e21c78f1200e
pyc-chan <rtrtoioil@gmail.com>
2024-11-25 17:00:59 +0900

    [Bronze V] Title: 개수 세기, Time: 180 ms, Memory: 17820 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
