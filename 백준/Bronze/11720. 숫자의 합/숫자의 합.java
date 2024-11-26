54ffd6ee3e52b46444996acaaba7829d68e83d3f
pyc-chan <rtrtoioil@gmail.com>
2024-11-26 11:16:58 +0900

    [Bronze IV] Title: 숫자의 합, Time: 176 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,12 +1,9 @@
 import java.util.Scanner;
-import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        StringTokenizer st;
-
-
         int n = scan.nextInt();
         String str = scan.next();
 
