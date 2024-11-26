778a971eb73f2178390aef53c13d84b9c210c9ff
pyc-chan <rtrtoioil@gmail.com>
2024-11-26 15:19:53 +0900

    [Bronze V] Title: 직사각형, Time: 176 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int su1 = scan.nextInt();
+        int su2 = scan.nextInt();
+        System.out.println(su1*su2);
+
+        scan.close();
+
+    }
+}
