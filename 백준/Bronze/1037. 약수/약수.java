4c986f39401f7824a6cd2475859b481832b82d43
pyc-chan <rtrtoioil@gmail.com>
2024-12-06 12:01:52 +0900

    [Bronze I] Title: 약수, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -1,32 +1,29 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
-            
             int su = Integer.parseInt(br.readLine());
             if (su == 1) {
                 int next = Integer.parseInt(br.readLine());
                 System.out.println(next * next);
             } else {
-                int[] yak = new int[su];
                 int min = 0;
                 int max = 0;
                 String[] str = br.readLine().split(" ");
                 for (int i = 0; i < su; i++) {
-                    yak[i] = Integer.parseInt(str[i]);
+                    int tmp = Integer.parseInt(str[i]);
                     if (i == 0) {
-                        min = yak[i];
-                        max = yak[i];
+                        min = tmp;
+                        max = tmp;
                     } else {
-                        if (yak[i] < min) {
-                            min = yak[i];
+                        if (tmp < min) {
+                            min = tmp;
+                        }else if(tmp> max){
+                            max = tmp;
                         }
-                    }if(yak[i]> max){
-                        max = yak[i];
                     }
                     
                 }
@@ -38,3 +35,4 @@ public class Main {
             
     }
 }
+    
