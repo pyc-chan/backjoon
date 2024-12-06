994c32ab269bf6c7720c84a1f5fb4ce6c540395e
pyc-chan <rtrtoioil@gmail.com>
2024-12-06 13:15:36 +0900

    [Bronze V] Title: A+B, Time: 108 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,11 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        System.out.print(a+b);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String[] str = br.readLine().split(" ");
+            System.out.print(Integer.parseInt(str[0])+Integer.parseInt(str[1]));
+            br.close();
+        } catch (Exception e) {
+        }
     }
 }
