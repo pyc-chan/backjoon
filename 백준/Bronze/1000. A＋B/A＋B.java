0e75224748c003ce805de5db6a68061e30255deb
pyc-chan <rtrtoioil@gmail.com>
2024-12-06 14:04:03 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,14 +1,7 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            String[] str = br.readLine().split(" ");
-            System.out.print(Integer.parseInt(str[0])+Integer.parseInt(str[1]));
-            br.close();
-        } catch (Exception e) {
-        }
+    public static void main(String[] args) throws IOException{
+        System.out.print(System.in.read()+System.in.read()+System.in.read()-' '-'0'-'0');
     }
 }
