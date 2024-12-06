3cf4fe0603dc54f06a534289f4384e0972f83956
pyc-chan <rtrtoioil@gmail.com>
2024-12-06 14:07:31 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -2,6 +2,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        System.out.print(System.in.read()+System.in.read()+System.in.read()-' '-'0'-'0');
+        System.out.print(System.in.read()+System.in.read()+System.in.read()-'0'-' '-'0');
     }
 }
