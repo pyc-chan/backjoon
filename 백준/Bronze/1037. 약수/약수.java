d06ac9ab3e6abfe70fcaa42ccb04ef5c0c982284
pyc-chan <rtrtoioil@gmail.com>
2024-12-06 11:55:56 +0900

    [Bronze I] Title: 약수, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -1,37 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int su = scan.nextInt();
-        if (su == 1) {
-            int next = scan.nextInt();
-            System.out.println(next * next);
-        } else {
-
-            int[] yak = new int[su];
-            int min = Integer.MAX_VALUE;
-            int max = 1;
-
-            for (int i = 0; i < su; i++) {
-                yak[i] = scan.nextInt();
-                if (i == 0) {
-                    min = yak[i];
-                } else {
-                    if (yak[i] < min) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            
+            int su = Integer.parseInt(br.readLine());
+            if (su == 1) {
+                int next = Integer.parseInt(br.readLine());
+                System.out.println(next * next);
+            } else {
+                int[] yak = new int[su];
+                int min = 0;
+                int max = 0;
+                String[] str = br.readLine().split(" ");
+                for (int i = 0; i < su; i++) {
+                    yak[i] = Integer.parseInt(str[i]);
+                    if (i == 0) {
                         min = yak[i];
+                        max = yak[i];
+                    } else {
+                        if (yak[i] < min) {
+                            min = yak[i];
+                        }
+                    }if(yak[i]> max){
+                        max = yak[i];
                     }
+                    
                 }
-                if(yak[i]> max){
-                    max = yak[i];
-                }
-
+                System.out.println(max*min);
             }
-            System.out.println(max*min);
+            br.close();
+        } catch (Exception e) {
         }
-
-        scan.close();
-
+            
     }
 }
