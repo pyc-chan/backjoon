3a0ce53396dcf15c17959f2d76804382274605b5
pyc-chan <rtrtoioil@gmail.com>
2025-02-14 09:10:12 +0900

    [Bronze I] Title: 약수, Time: 52 ms, Memory: 5572 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.cs
+++ 백준/Bronze/1037. 약수/약수.cs
@@ -2,19 +2,11 @@ int num = int.Parse(Console.ReadLine());
 string[] strs = Console.ReadLine().Split();
 int max = 1;
 int min = 1000000;
-if (strs.Length > 1)
-{
-    for (int i = 0; i < strs.Length; i++)
+for (int i = 0; i < strs.Length; i++)
     {
         int Val = int.Parse(strs[i]);
         if(Val > max) max = Val;
         if(Val < min) min = Val;
     }
-}
-else
-{
-    max = int.Parse(strs[0]);
-    min = int.Parse(strs[0]);
-}
 int result = max * min;
 Console.WriteLine(result);
