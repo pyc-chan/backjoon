81e8bd78acba61ffdd343a836627805399b8c424
pyc-chan <rtrtoioil@gmail.com>
2025-03-07 00:45:10 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 96 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+public class Main {
+    public static void main(String[] args)throws IOException{
+        System.out.println("2025");
+        System.out.println("03");
+        System.out.println("06");
+    }
+}
